2fee444eb4a9e17e9cb8ba2602c1743d7583373f
Dilan <dilhan.kosekul@gmail.com>
2021-01-19 23:07:46 +0300

    silindi

--- ExaminatioSystemProject/src/exam/Main.java
+++ ExaminatioSystemProject/src/exam/Main.java
@@ -20,7 +20,5 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    public void test() {
-    	System.out.println("test");
-    }
+   
 }
